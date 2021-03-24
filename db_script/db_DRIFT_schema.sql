247f52e4191393c3a6743d09c6b584e8530963c5
mahbubur.rahman <blackmahbub@gmail.com>
2021-03-24 11:06:43 +0100

    update db modeling

--- db_script/db_DRIFT_schema.sql
+++ db_script/db_DRIFT_schema.sql
@@ -30,9 +30,44 @@ CREATE TABLE `tbl_product` (
   `brand` varchar(45) NOT NULL,
   `seller` varchar(45) NOT NULL,
   `shop_name` varchar(45) NOT NULL DEFAULT 'Amazon',
+  `category` varchar(45) NOT NULL DEFAULT 'missing',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_product`
+--
+
+LOCK TABLES `tbl_product` WRITE;
+/*!40000 ALTER TABLE `tbl_product` DISABLE KEYS */;
+INSERT INTO `tbl_product` VALUES (2,'test',24.02,'Euro','Amazon','Amazon','Amazon','missing'),(3,'test',24.02,'Euro','Amazon','Amazon','Amazon','missing'),(4,'test',24.02,'Euro','Amazon','Amazon','Amazon','missing'),(5,'test',24.02,'Euro','Amazon','Amazon','Amazon','missing');
+/*!40000 ALTER TABLE `tbl_product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_product_vocabulary`
+--
+
+DROP TABLE IF EXISTS `tbl_product_vocabulary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_product_vocabulary` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int NOT NULL,
+  `vocabulary_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_product_vocabulary`
+--
+
+LOCK TABLES `tbl_product_vocabulary` WRITE;
+/*!40000 ALTER TABLE `tbl_product_vocabulary` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_product_vocabulary` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tbl_review`
@@ -49,11 +84,77 @@ CREATE TABLE `tbl_review` (
   `product_id` int NOT NULL,
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `locale` varchar(6) NOT NULL DEFAULT 'en_US',
   PRIMARY KEY (`id`),
   KEY `fk_tbl_review_1_idx` (`product_id`),
   CONSTRAINT `fk_tbl_review_1` FOREIGN KEY (`product_id`) REFERENCES `tbl_product` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_review`
+--
+
+LOCK TABLES `tbl_review` WRITE;
+/*!40000 ALTER TABLE `tbl_review` DISABLE KEYS */;
+INSERT INTO `tbl_review` VALUES (2,'Sexy LED Strips','The price seems to be higher than its features. However, the product is good and the most important thing that I satisfy using this product. So I give this product 5 stars.','SUBMIT',2,'2021-03-21 02:43:21','2021-03-21 02:43:21','en_US'),(4,'High quality iPhone replacement Battery','This product is rated 5 stars because it has some additional features compared to other Batteries. These extra features are High Capacity, Tool Kit and Repair Kit.\n\nThere are no issues with the product and the product fits properly on my iPhone. However, at first it is not working. Later it is successful with the help of Battery Replacement Instructions provided by the seller, so thanks to the seller for the Battery Replacement Instructions to be attached with the package.\n\nBecause of the smart functionalities and better quality, I like this product very much and at the same time I recommend future buyers to buy this product for their iPhone.','SUBMIT',4,'2021-03-24 09:26:47','2021-03-24 09:26:47','en_US'),(5,'Smart and Sexy LED Strips','Because of the smart and sexy look with excellent App Control, I give this product 5 stars. Compared to other LED Strips, this product has a Remote Control, RGB, USB Operated, is usefull, is good for decoration, and has a low electricity consumption.\n\nThere are no issues with the product, and the product fits my Wall Mirror just right. In addition, this product includes built-in high-sensitivity microphone, 16 million colours for brightness.\n\nBecause of the smart functionalities and better quality, I really like this product and recommend future buyers to buy this product.','SUBMIT',5,'2021-03-24 09:38:49','2021-03-24 09:38:49','en_US');
+/*!40000 ALTER TABLE `tbl_review` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_review_vocabulary`
+--
+
+DROP TABLE IF EXISTS `tbl_review_vocabulary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_review_vocabulary` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `review_id` int NOT NULL,
+  `vocabulary_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_review_vocabulary`
+--
+
+LOCK TABLES `tbl_review_vocabulary` WRITE;
+/*!40000 ALTER TABLE `tbl_review_vocabulary` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_review_vocabulary` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_vocabulary`
+--
+
+DROP TABLE IF EXISTS `tbl_vocabulary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_vocabulary` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `word` varchar(69) NOT NULL,
+  `synonym_1` varchar(69) NOT NULL,
+  `synonym_2` varchar(69) NOT NULL,
+  `synonym_3` varchar(69) DEFAULT NULL,
+  `synonym_4` varchar(69) DEFAULT NULL,
+  `synonym_5` varchar(69) DEFAULT NULL,
+  `synonym_6` varchar(69) DEFAULT NULL,
+  `meaning` varchar(69) NOT NULL,
+  `locale` varchar(6) NOT NULL DEFAULT 'en_US',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_vocabulary`
+--
+
+LOCK TABLES `tbl_vocabulary` WRITE;
+/*!40000 ALTER TABLE `tbl_vocabulary` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_vocabulary` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -64,4 +165,4 @@ CREATE TABLE `tbl_review` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-21  2:49:04
+-- Dump completed on 2021-03-24 11:04:34
